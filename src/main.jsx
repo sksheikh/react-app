eb5fc1177bad83cdb1595ed0cd531e6742494850
Skuddin <sheikh@nextgenitltd.com>
2023-12-05 17:48:36 +0600

    css in jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,16 @@ import ReactDOM from 'react-dom/client'
 // }
 
 const title = 'Hello React';
-const description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quas?';
+const cardTitle = 'Card Title';
+const description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quas? Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quas?';
 const date = new Date().getFullYear();
 ReactDOM.createRoot(document.getElementById('root')).render(
  <div>
     <h1 className='headingStyle'>{title}</h1>
-    <p>{description}</p>
-    <p>{date}</p>
+    <div className='card'>
+      <h2 className='cardTitle'>{cardTitle}</h2>
+      <p className='cardDescription'>{description}</p>
+      <p className='cardFooter'>{date}</p>
+    </div>
  </div>
 )
