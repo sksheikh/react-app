6c0eafa57f2968d803b831ea0ab17ec7838a2e5d
Skuddin <sheikh@nextgenitltd.com>
2023-12-09 16:06:03 +0600

    conditional rendering

--- 02.react-icon/src/App.jsx
+++ 02.react-icon/src/App.jsx
@@ -1,35 +1,11 @@
-import React, { Component } from 'react'
-import './index.css'
+import React from 'react'
+import ConditionaRendering from './Components/ConditionalRendering'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       count: 0
-    }
-  }
-
-  handleIncrement = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  };
-
-  handleDecrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
-  };
-  
-  render() {
-    const {count} =this.state
-    return (
-      <div>
-        <p>Count : {count}</p>
-        <button onClick={this.handleIncrement} disabled={count===5}> + </button>
-        <button onClick={this.handleDecrement} disabled={count===0}> - </button>
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div>
+      <ConditionaRendering />
+    </div>
+  )
 }
+
