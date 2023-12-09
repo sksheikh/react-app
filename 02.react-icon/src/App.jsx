74dc3b29d24638aa9e363878735afe805247380b
Skuddin <sheikh@nextgenitltd.com>
2023-12-09 14:53:37 +0600

    react icon

---
+++ 02.react-icon/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { FaFacebook, FaLinkedin  } from "react-icons/fa6";
+
+function App() {
+
+  return (
+    <>
+      <h1>Reac Icon</h1>
+      <FaFacebook className='icon'/>
+      <FaLinkedin className='icon' />
+    </>
+  )
+}
+
+export default App
