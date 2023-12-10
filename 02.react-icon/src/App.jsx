b7d6005e209482180b8255aee9d887820e39af40
sksheikh <skuddin.ius@gmail.com>
2023-12-10 21:08:25 +0600

    event bubbling stop propagation

--- 02.react-icon/src/App.jsx
+++ 02.react-icon/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import EventHandlerClass from './EVENT_HANDLER_CLASS'
 
 export default function App() {
+  const handleParentClick = (event) => {
+    console.log('Parent event:', event);
+  }
+
+  const handleChildClick = (event) => {
+    event.stopPropagation();
+    console.log('Child event:',event);
+  }
   return (
-    <div>
-      <EventHandlerClass />
+    <div onClick={handleParentClick}>
+      <h1>Welcome React</h1>
+      <button onClick={handleChildClick}>Click here</button>
     </div>
   )
 }
