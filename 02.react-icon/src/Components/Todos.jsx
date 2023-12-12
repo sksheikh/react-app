700d094e7974bd637d8ec8ada815a37e12c2ecf2
Skuddin <sheikh@nextgenitltd.com>
2023-12-12 15:14:23 +0600

    form validation with yup

--- 02.react-icon/src/Components/Todos.jsx
+++ 02.react-icon/src/Components/Todos.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Todo from './Todo'
+import { FaRegTrashCan } from "react-icons/fa6";
 
 export default function Todos({todos}) {
   return (
     <div>
       {todos.map((todo, index) => 
         <Todo key={index} todo={todo} />)}
+
+        <button>
+          <FaRegTrashCan />
+        </button>
     </div>
   )
 }
