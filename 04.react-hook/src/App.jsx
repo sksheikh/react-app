6ddf71a44e5824737d3f9b1f00114279c7637259
Skuddin <sheikh@nextgenitltd.com>
2023-12-14 17:45:04 +0600

    useEffect hook & fetch data

--- 04.react-hook/src/App.jsx
+++ 04.react-hook/src/App.jsx
@@ -5,18 +5,31 @@ import './App.css'
 
 function App() {
   const [todos, setTodo] = useState(null)
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
 
-  
-    useEffect(() =>{
-          fetch('https://jsonplaceholder.typicode.com/todos')
-          .then((res) => {
-            return res.json()
-          })
-          .then((data) => {
-            setTodo(data)
-          })
-      },[]);
+  const loadingMessage = 'Loading...'
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        if(!res.ok){
+          throw Error('Data not found')
+        }
+        return res.json()
+      })
+      .then((data) => {
+        setTodo(data)
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      })
+  }, []);
+
+  const renderTodos = todos && todos.map((todo) =>
+  <p key={todo.id}>{todo.title}</p>)
   // const handleCount = () => {
   //   console.log('useState');
   //   setCount(count + 1);
@@ -25,10 +38,12 @@ function App() {
   return (
     <>
       <h1>Todos</h1>
-      { todos && todos.map((todo) =>
-      <p key={todo.id}>{todo.title}</p>)
-      }
-      
+      {isLoading ?
+       loadingMessage :
+       renderTodos }
+       {error}
+      {/* {renderTodos} */}
+
     </>
   )
 }
