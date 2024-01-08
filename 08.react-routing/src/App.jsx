6691a9f26e9c2be22c38136b99b71806ca28773d
Skuddin <sheikh@nextgenitltd.com>
2024-01-08 12:59:55 +0600

    navigation and redirect

--- 08.react-routing/src/App.jsx
+++ 08.react-routing/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import './App.css'
+// import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Error from './pages/Error'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <BrowserRouter >
+        <Navbar />
         <Routes >
           <Route path='/' element={<Home />} />
           <Route path='/contact' element={<Contact />} />
---
+++ 08.react-routing/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <NavLink className="nav-link" to={'/'}>Home</NavLink>
+      <NavLink className="nav-link" to={'/contact'}>Contact</NavLink>
+    </nav>
+  )
+}
+
