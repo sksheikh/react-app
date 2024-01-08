11d6310e83986dba20c3a7ff83d6de411b116313
Skuddin <sheikh@nextgenitltd.com>
2024-01-08 17:26:19 +0600

    use useLocation

--- 08.react-routing/src/pages/Blog.jsx
+++ 08.react-routing/src/pages/Blog.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from 'react'
 
-import {blogs} from '../blogsData.js'
-import { useParams } from 'react-router-dom'
+// import {blogs} from '../blogsData.js'
+import { useLocation, useParams } from 'react-router-dom'
 
 export default function Blog() {
-  const param = useParams()
-  const {title} = param
-  const [blogData, setBlogData] = useState('');
+  const param = useParams();
+  const {title} = param;
+  // const [blogData, setBlogData] = useState('');
+  const location = useLocation();
 
-  useEffect(()=> {
-    const filteredBlog = blogs.filter((blog) => {
-      return blog.title === title
-    })
-    setBlogData(filteredBlog[0])
-  },[])
+  // useEffect(()=> {
+  //   const filteredBlog = blogs.filter((blog) => {
+  //     return blog.title === title
+  //   })
+  //   setBlogData(filteredBlog[0])
+  // },[])
   return (
     <div>
       <h2>Single blog</h2>
-      <h4>Title: {blogData.title}</h4>
-      <p>Description: {blogData.description}</p>
+      <h4>Title: {location.state.title}</h4>
+      <p>Description: {location.state.description}</p>
     </div>
   )
 }
